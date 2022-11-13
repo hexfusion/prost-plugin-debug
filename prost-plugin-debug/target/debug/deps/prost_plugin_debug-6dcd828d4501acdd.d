7f36c7cc8e0ee7a8717812216b8475eaf6be13d5
Sam Batschelet <sam.batschelet@avalabs.org>
2022-11-13 14:49:28 -0500

    min example
    Signed-off-by: Sam Batschelet <sam.batschelet@avalabs.org>

---
+++ prost-plugin-debug/target/debug/deps/prost_plugin_debug-6dcd828d4501acdd.d
@@ -0,0 +1,5 @@
+/home/sam.batschelet/projects/hexfusion/prost-plugin-debug/prost-plugin-debug/prost-plugin-debug/target/debug/deps/prost_plugin_debug-6dcd828d4501acdd.rmeta: src/main.rs
+
+/home/sam.batschelet/projects/hexfusion/prost-plugin-debug/prost-plugin-debug/prost-plugin-debug/target/debug/deps/prost_plugin_debug-6dcd828d4501acdd.d: src/main.rs
+
+src/main.rs:
---
+++ prost-plugin-debug/target/debug/deps/prost_plugin_debug-bf95a20d9e089ff9.d
@@ -0,0 +1,5 @@
+/home/sam.batschelet/projects/hexfusion/prost-plugin-debug/prost-plugin-debug/prost-plugin-debug/target/debug/deps/prost_plugin_debug-bf95a20d9e089ff9.rmeta: src/main.rs
+
+/home/sam.batschelet/projects/hexfusion/prost-plugin-debug/prost-plugin-debug/prost-plugin-debug/target/debug/deps/prost_plugin_debug-bf95a20d9e089ff9.d: src/main.rs
+
+src/main.rs:
